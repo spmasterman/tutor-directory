869df47b762bfff6231371175be1435e42cce436
Shaun Masterman <shaun@masterman.com>
2014-11-14 12:42:52 +0000

    - Security in MenuBuilder

--- src/Fitch/FrontEndBundle/Menu/MenuBuilder.php
+++ src/Fitch/FrontEndBundle/Menu/MenuBuilder.php
@@ -25,6 +25,7 @@ class MenuBuilder extends ContainerAware
             ->addFileTypes($menu)
             ->addCompetencyTypes($menu)
             ->addCompetencyLevels($menu)
+            ->addUsers($menu)
         ;
 
         return $menu;
@@ -51,6 +52,7 @@ class MenuBuilder extends ContainerAware
             ->addFileTypes($homeNode)
             ->addCompetencyTypes($homeNode)
             ->addCompetencyLevels($homeNode)
+            ->addUsers($homeNode)
         ;
 
         return $menu;
@@ -62,13 +64,7 @@ class MenuBuilder extends ContainerAware
                         ->setChildrenAttribute('class', 'dropdown-menu')
                         ->setChildrenAttribute('role', 'menu');
 
-        $menu->addChild('Profile', array('route' => 'fos_user_profile_show'))
-                ->setAttribute('icon', 'fa fa-user fa-fw')
-                ->getParent()
-             ->addChild('Settings', array('route' => 'home'))
-                ->setAttribute('icon', 'fa fa-cog fa-fw')
-                ->getParent()
-             ->addChild('Logout', array('route' => 'fos_user_security_logout'))
+        $menu->addChild('Logout', array('route' => 'fos_user_security_logout'))
                 ->setAttribute('icon', 'fa fa-power-off fa-fw')
                 ->getParent();
         return $menu;
@@ -81,10 +77,11 @@ class MenuBuilder extends ContainerAware
      */
     private function addTutorTypes(ItemInterface $menu)
     {
-        $menu
-            ->addChild('Tutor Types', array('route' => 'tutor_type'))
-            ->setAttribute('icon', 'fa fa-child fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('Tutor Types', array('route' => 'tutor_type'))
+                ->setAttribute('icon', 'fa fa-child fa-fw');
+        }
 
         return $this;
     }
@@ -95,10 +92,11 @@ class MenuBuilder extends ContainerAware
      */
     private function addStatus(ItemInterface $menu)
     {
-        $menu
-            ->addChild('Statuses', array('route' => 'status'))
-            ->setAttribute('icon', 'fa fa-tag fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('Statuses', array('route' => 'status'))
+                ->setAttribute('icon', 'fa fa-tag fa-fw');
+        }
 
         return $this;
     }
@@ -109,10 +107,11 @@ class MenuBuilder extends ContainerAware
      */
     private function addRegions(ItemInterface $menu)
     {
-        $menu
-            ->addChild('Regions', array('route' => 'region'))
-            ->setAttribute('icon', 'fa fa-globe fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('Regions', array('route' => 'region'))
+                ->setAttribute('icon', 'fa fa-globe fa-fw');
+        }
 
         return $this;
     }
@@ -123,10 +122,11 @@ class MenuBuilder extends ContainerAware
      */
     private function addCountries(ItemInterface $menu)
     {
-        $menu
-            ->addChild('Countries', array('route' => 'country'))
-            ->setAttribute('icon', 'fa fa-flag-o fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('Countries', array('route' => 'country'))
+                ->setAttribute('icon', 'fa fa-flag-o fa-fw');
+        }
 
         return $this;
     }
@@ -137,10 +137,11 @@ class MenuBuilder extends ContainerAware
      */
     private function addCurrencies(ItemInterface $menu)
     {
-        $menu
-            ->addChild('Currencies', array('route' => 'currency'))
-            ->setAttribute('icon', 'fa fa-money fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('Currencies', array('route' => 'currency'))
+                ->setAttribute('icon', 'fa fa-money fa-fw');
+        }
 
         return $this;
     }
@@ -151,10 +152,11 @@ class MenuBuilder extends ContainerAware
      */
     private function addFileTypes(ItemInterface $menu)
     {
-        $menu
-            ->addChild('File Types', array('route' => 'file_type'))
-            ->setAttribute('icon', 'fa fa-files-o fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('File Types', array('route' => 'file_type'))
+                ->setAttribute('icon', 'fa fa-files-o fa-fw');
+        }
 
         return $this;
     }
@@ -165,10 +167,11 @@ class MenuBuilder extends ContainerAware
      */
     private function addCompetencyTypes(ItemInterface $menu)
     {
-        $menu
-            ->addChild('Competency Types', array('route' => 'competency_type'))
-            ->setAttribute('icon', 'fa fa-bullseye fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('Competency Types', array('route' => 'competency_type'))
+                ->setAttribute('icon', 'fa fa-bullseye fa-fw');
+        }
 
         return $this;
     }
@@ -179,12 +182,23 @@ class MenuBuilder extends ContainerAware
      */
     private function addCompetencyLevels(ItemInterface $menu)
     {
-        $menu
-            ->addChild('Competency Levels', array('route' => 'competency_level'))
-            ->setAttribute('icon', 'fa fa-signal fa-fw')
-        ;
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $menu
+                ->addChild('Competency Levels', array('route' => 'competency_level'))
+                ->setAttribute('icon', 'fa fa-signal fa-fw');
+        }
+
+        return $this;
+    }
+
+    private function addUsers(ItemInterface $menu)
+    {
+        if (false !== $this->container->get('security.context')->isGranted('ROLE_SUPER_ADMIN')) {
+            $menu
+                ->addChild('User Management', array('route' => 'competency_level'))
+                ->setAttribute('icon', 'fa fa-users fa-fw');
+        }
 
         return $this;
     }
 }
-
