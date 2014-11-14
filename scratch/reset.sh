40bc0ba093715fecf4e0e1c7903065f053dc854e
Shaun Masterman <shaun@masterman.com>
2014-11-14 10:09:52 +0000

    - Test Users

--- scratch/reset.sh
+++ scratch/reset.sh
@@ -4,7 +4,15 @@ sudo truncate -s0 app/logs/dev.log
 php app/console doctrine:database:drop --force
 php app/console doctrine:database:create
 php app/console doctrine:schema:update --force
-php app/console khepin:yamlfixtures:load dev
 
 php app/console fos:user:create smasterman shaun@masterman.com xxx --super-admin
+php app/console fos:user:create xuser shaun@masterman.com xxx
+php app/console fos:user:promote xuser ROLE_USER
+
+php app/console fos:user:create xeditorshaun@masterman.com xxx
+php app/console fos:user:promote xeditor ROLE_EDITOR
+
+php app/console fos:user:create xadmin shaun@masterman.com xxx
+php app/console fos:user:promote xadmin ROLE_ADMIN
+
 
