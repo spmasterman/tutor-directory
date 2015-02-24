4863985588f2dbdbab4d69697b2d792a7559a92e
Shaun Masterman <shaun@masterman.com>
2015-02-24 19:46:49 +0000

    - reduce complexity of Attribute Subscriber

--- src/Fitch/EntityAttributeValueBundle/Form/EventListener/AttributeSubscriber.php
+++ src/Fitch/EntityAttributeValueBundle/Form/EventListener/AttributeSubscriber.php
@@ -3,10 +3,11 @@ namespace Fitch\EntityAttributeValueBundle\Form\EventListener;
 
 use Fitch\EntityAttributeValueBundle\Entity\Attribute;
 use Fitch\EntityAttributeValueBundle\Entity\Option;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
 
 class AttributeSubscriber implements EventSubscriberInterface
 {
@@ -48,81 +49,26 @@ class AttributeSubscriber implements EventSubscriberInterface
         $data = $event->getData();
         $form = $event->getForm();
 
-        if (null === $data) {
+        if (null === $data || $data->getDefinition()) {
             return;
         }
 
         $this->createValueField($data, $form);
     }
 
-    public function createValueField($data, $form, $fieldName = 'value')
+    public function createValueField(Attribute $attribute, FormInterface $form, $fieldName = 'value')
     {
-        if ($data->getDefinition() == null) {
-            return false;
-        }
-        /** @var Attribute $attribute */
-        $attribute = $data;
         $definition = $attribute->getDefinition();
 
-        $type = $definition->getType();
-        /** @var Option[] $options */
-        $options = $definition->getOptions()->toArray();
-        $value = $attribute->getValue();
-
-        $params = array(
-            'attr' => array(
-
-            ),
+        list($type, $params, $value) = $this->dealWithTypes(
+            $definition->getType(),
+            ['attr' => []],
+            $attribute->getValue(),
+            $definition->getOptions()->toArray()
         );
 
-        if ($type == 'textarea' && !$this->getOption('allow_expanded')) {
-            $type = 'text';
-        }
-
-        if ($type == 'choice' || $type == 'checkbox' || $type == 'radio') {
-            if (($type == 'checkbox' || $type == 'radio') && $this->getOption('allow_expanded')) {
-                $params['expanded'] = true;
-            }
-
-            if ($this->getOption('all_multiple')) {
-                $params['multiple'] = true;
-            } else {
-                if ($type == 'radio') {
-                    $params['multiple'] = false;
-                } elseif ($type == 'checkbox') {
-                    if (!is_array($value)) {
-                        $value = array(
-                            $value => $value,
-                        );
-                    }
-
-                    $params['multiple'] = true;
-                }
-            }
-
-            $params['choices'] = array();
-
-            foreach ($options as $option) {
-                $params['choices'][$option->getName()] = $option->getName();
-            }
-
-            $type = 'choice';
-        } elseif (is_array($value)) {
-            $value = null;
-        }
-
-        if ($definition->getRequired() == true) {
-            $params['required'] = true;
-        } else {
-            $params['required'] = false;
-        }
-
-        $label = $definition->getLabel();
-        if (!$label) {
-            $label = $definition->getName();
-        }
-
-        $params['label'] = $label;
+        $params['required'] = (bool)$definition->getRequired();
+        $params['label'] = $definition->getLabel() ? $definition->getLabel() : $definition->getName();
 
         if ($definition->getUnit() != "") {
             $params['label_attr']['unit'] = $definition->getUnit();
@@ -135,5 +81,63 @@ class AttributeSubscriber implements EventSubscriberInterface
         $params['auto_initialize'] = false;
 
         $form->add($this->factory->createNamed($fieldName, $type, $value, $params));
+    }
+
+    /**
+     * @param $type
+     * @param $params
+     * @param $value
+     * @param $options
+     * @return array
+     */
+    private function dealWithTypes($type, $params, $value, $options)
+    {
+        if ($type == 'textarea' && !$this->getOption('allow_expanded')) {
+            $type = 'text';
+        }
+
+        if ($type == 'choice' || $type == 'checkbox' || $type == 'radio') {
+            return $this->dealWithChoice($type, $params, $value, $options);
+        }
+
+        return array($type, $params, is_array($value) ? null : $value);
+    }
+
+    /**
+     * @param $type
+     * @param $params
+     * @param $value
+     * @param $options
+     * @return array
+     */
+    private function dealWithChoice($type, $params, $value, $options)
+    {
+        if (($type == 'checkbox' || $type == 'radio') && $this->getOption('allow_expanded')) {
+            $params['expanded'] = true;
+        }
+
+        if ($this->getOption('all_multiple')) {
+            $params['multiple'] = true;
+        } else {
+            if ($type == 'radio') {
+                $params['multiple'] = false;
+            } elseif ($type == 'checkbox') {
+                if (!is_array($value)) {
+                    $value = array(
+                        $value => $value,
+                    );
+                }
+                $params['multiple'] = true;
+            }
+        }
+
+        $params['choices'] = array();
+        foreach ($options as $option) {
+            /** @var Option $option */
+            $params['choices'][$option->getName()] = $option->getName();
+        }
+
+        $type = 'choice';
+        return array($type, $params, $value);
     }
 }
