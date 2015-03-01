1c23f5d8c3221133651c86cb28499a9e542a2325
Shaun Masterman <shaun@masterman.com>
2015-03-01 19:29:18 +0000

    - TutorTypeManager Test Case

--- src/Fitch/TutorBundle/Tests/Model/TutorTypeManagerTest.php
+++ src/Fitch/TutorBundle/Tests/Model/TutorTypeManagerTest.php
@@ -3,79 +3,93 @@
 namespace Fitch\TutorBundle\Tests\Model;
 
 use Fitch\CommonBundle\Model\FixturesWebTestCase;
+use Fitch\CommonBundle\Tests\Model\ChoicesModelManagerTestTrait;
+use Fitch\CommonBundle\Tests\Model\DefaultableModelManagerTestTrait;
+use Fitch\CommonBundle\Tests\Model\FindModelManagerTestTrait;
+use Fitch\CommonBundle\Tests\Model\TimestampableModelManagerTestTrait;
+use Fitch\TutorBundle\Entity\TutorType;
 use Fitch\TutorBundle\Model\TutorTypeManagerInterface;
 
 class TutorTypeManagerTest extends FixturesWebTestCase
 {
-    public function testFindAll()
+    use TimestampableModelManagerTestTrait,
+        ChoicesModelManagerTestTrait,
+        FindModelManagerTestTrait,
+        DefaultableModelManagerTestTrait;
+
+    const FIXTURE_COUNT = 3;
+
+    /** @var  TutorTypeManagerInterface */
+    protected $modelManager;
+
+    /**
+     * Runs for every test.
+     */
+    public function setUp()
     {
-        $allEntities = $this->getModelManager()->findAll();
-        $this->assertCount(3, $allEntities, "Should return three file entities");
-
-        $this->assertEquals('Test Tutor Type One', $allEntities[0]->getName());
-        $this->assertEquals('Test Tutor Type Two', $allEntities[1]->getName());
-        $this->assertEquals('Test Tutor Type Three', $allEntities[2]->getName());
-    }
-
-    public function testFindById()
-    {
-        $entityOne = $this->getModelManager()->findById(1);
-
-        $this->assertEquals('Test Tutor Type One', $entityOne->getName());
-    }
-
-    public function testLifeCycle()
-    {
-        // Check that there are 3 entries
-        $allEntities = $this->getModelManager()->findAll();
-        $this->assertCount(3, $allEntities, "Should return three entities");
-
-        // Create new one
-        $newEntity = $this->getModelManager()->createEntity();
-        $newEntity
-            ->setName('t')
-            ->setDefault(false)
-        ;
-        $this->getModelManager()->saveEntity($newEntity);
-
-        // Check that there are 4 entries, and the new one is Timestamped correctly
-        $allEntities = $this->getModelManager()->findAll();
-        $this->assertCount(4, $allEntities, "Should return four entities");
-        $this->assertNotNull($allEntities[3]->getCreated());
-        $this->assertEquals($allEntities[3]->getCreated(), $allEntities[3]->getUpdated());
-
-        // Updated shouldn't change until persisted
-        $newEntity->setName('t2');
-        $this->assertEquals($allEntities[3]->getCreated(), $allEntities[3]->getUpdated());
-
-        sleep(1);
-
-        $this->getModelManager()->saveEntity($newEntity);
-        $this->assertNotEquals($allEntities[3]->getCreated(), $allEntities[3]->getUpdated());
-
-        // Check that when we refresh it refreshes
-        $newEntity->setName('t3');
-        $this->getModelManager()->reloadEntity($newEntity);
-        $this->assertEquals('t2', $newEntity->getName());
-
-        // Check that when we remove it, it is no longer present
-        $this->getModelManager()->removeEntity($newEntity);
-        $allEntities = $this->getModelManager()->findAll();
-        $this->assertCount(3, $allEntities, "Should return three entities");
-    }
-
-    public function testFindDefaultFileType()
-    {
-        $entity = $this->getModelManager()->findDefaultEntity();
-
-        $this->assertTrue($entity->isDefault());
+        parent::setUp();
+        $this->modelManager = $this->container->get('fitch.manager.tutor_type');
     }
 
     /**
-     * @return TutorTypeManagerInterface
+     * Tests the findAll.
      */
-    public function getModelManager()
+    public function testFindAll()
     {
-        return $this->container->get('fitch.manager.tutor_type');
+        $this->performFindAllTest(self::FIXTURE_COUNT, 'Test Tutor Type One', function (TutorType $entity) {
+            return $entity->getName();
+        });
+    }
+
+    /**
+     *
+     */
+    public function testFindById()
+    {
+        $this->performFindByIdTest(1, 'Test Tutor Type One', function (TutorType $entity) {
+            return $entity->getName();
+        });
+    }
+
+    /**
+     *
+     */
+    public function testLifeCycle()
+    {
+        $this->performLifeCycleTests(
+            self::FIXTURE_COUNT,
+            function (TutorType $entity) {
+                $entity
+                    ->setName('n')
+                    ->setDefault(false)
+                ;
+            },
+            function (TutorType $entity) {
+                $entity
+                    ->setName('p2');
+            },
+            function (TutorType $entity) {
+                $entity
+                    ->setName('p3');
+            },
+            function (TutorType $entity) {
+                return (bool) 'p2' == $entity->getName();
+            }
+        );
+    }
+
+    /**
+     *
+     */
+    public function testFindDefault()
+    {
+        $this->performFindDefaultTest(
+            function ($entity) {
+                return $entity instanceof TutorType;
+            },
+            function (TutorType $entity) {
+                return $entity->isDefault();
+            }
+        );
     }
 }
