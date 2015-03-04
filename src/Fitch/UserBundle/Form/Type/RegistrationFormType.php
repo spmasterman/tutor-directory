d3a03c3ce75c51c5549921807dadc5db5d220d13
Shaun Masterman <shaun@masterman.com>
2015-03-04 23:04:06 +0000

    - RegistrationFormType revert

--- src/Fitch/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Fitch/UserBundle/Form/Type/RegistrationFormType.php
@@ -5,22 +5,36 @@ namespace Fitch\UserBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+/**
+ * Class RegistrationFormType.
+ */
 class RegistrationFormType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $options = null;
-        // add your custom field
-        $builder->add('fullName', null, [
-            'label'=>'full name'
-        ]);
+        // add custom field
+        $builder
+            ->add('fullName', null, [
+                'label' => 'full name',
+            ]);
     }
 
+    /**
+     * @return string
+     */
     public function getParent()
     {
         return 'fos_user_registration';
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'fitch_user_registration';
