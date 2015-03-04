f6d4a5785fb8ec9909b3f61f778a937f4198899a
Shaun Masterman <shaun@masterman.com>
2015-03-04 01:08:28 +0000

    - NavigationControllerTest proper namespacing

--- src/Fitch/FrontEndBundle/Tests/Controller/NavigationControllerTest.php
+++ src/Fitch/FrontEndBundle/Tests/Controller/NavigationControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Fitch\TutorBundle\Tests\Controller;
+namespace Fitch\FrontEndBundle\Tests\Controller;
 
 use Fitch\CommonBundle\Tests\AuthorisedClientTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
